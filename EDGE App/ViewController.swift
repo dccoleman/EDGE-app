14afc996489311a1426bfe42f1aa248e560402df
dccoleman <dccoleman@wpi.edu>
2015-10-27 14:10:22 -0400

    Initial Commit

---
+++ EDGE App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EDGE App
+//
+//  Created by Devon Coleman on 10/27/15.
+//  Copyright © 2015 EDGE IQP Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
