e9217e08e51e4a2657d4b1094fec15eb5930580b
dccoleman <dccoleman@wpi.edu>
2015-11-23 21:28:53 -0500

    I tried

---
+++ SampleData.swift
@@ -0,0 +1,15 @@
+//
+//  SampleData.swift
+//  EDGE App
+//
+//  Created by Devon Coleman on 11/23/15.
+//  Copyright © 2015 EDGE IQP Team. All rights reserved.
+//
+
+import Foundation
+
+let appsData = [
+    Entry(label: "Timer", url: "www.timer.com"),
+    Entry(label: "Calendar", url: "Apple Calendar"),
+    Entry(label: "Football Games", url: "WPI Athletics")
+]
