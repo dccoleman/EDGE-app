a1dbf0322de0652cba51baf60df098b0107bbf47
dccoleman <dccoleman@wpi.edu>
2015-11-23 22:45:21 -0500

    Finished loading from memory, TODO list added

---
+++ EDGE App/SampleData.swift
@@ -0,0 +1,15 @@
+//
+//  SampleData.swift
+//  EDGE App
+//
+//  Created by Devon Coleman on 11/23/15.
+//  Copyright © 2015 EDGE IQP Team. All rights reserved.
+//
+
+import Foundation
+
+let appsData = [
+    Entry(label: "Timer", url: "www.clock.com"),
+    Entry(label: "Football Games", url: "www.wpi.com/+sports"),
+    Entry(label: "Study Guide", url: "www.sparknotes.com")
+]
